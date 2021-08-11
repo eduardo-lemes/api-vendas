7bed8c9407e8a934719eb5a941bdf4777307241f
eduardoLemes <eduardo_lsf@outlook.com>
2021-08-11 16:06:58 -0300

    controller and  methods

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,7 +3,9 @@ import { Router } from 'express';
 const routes = Router();
 
 routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello Dev' });
+  return response.json({
+    message: 'Rota teste hello',
+  });
 });
 
 export default routes;
